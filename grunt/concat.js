e6ff094590cf173803cb60c0cbf545738b7bfa8f
Adam Whittingham <adam.whittingham@gmail.com>
2014-11-17 16:52:03 +0000

    Move before.js inclusion to around the src code only

--- grunt/concat.js
+++ grunt/concat.js
@@ -1,9 +1,9 @@
 module.exports = {
   dist: {
     src: [
-      'js/src/before.js',
       'js/lib/jquery.js',
       'js/lib/*.js',
+      'js/src/before.js',
       'js/src/*.js',
       'js/src/after.js'
     ],
