12670613c50653c251c32672d61f31dab7cf87da
Adam Whittingham <adam.whittingham@gmail.com>
2014-12-03 19:52:02 +0000

    Add clean images

--- grunt/clean.js
+++ grunt/clean.js
@@ -2,5 +2,6 @@ module.exports = {
   js: [ 'dist/*.js' ],
   css: [ 'dist/*.css' ],
   html: [ 'dist/*.html' ],
-  map: [ 'dist/*.map']
+  map: [ 'dist/*.map'],
+  images: [ 'dist/images/**/*' ]
 };
