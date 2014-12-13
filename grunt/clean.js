cb8e5dd6080dbc90b9c359d50104c0ee448c432e
Adam Whittingham <adam.whittingham@gmail.com>
2014-12-13 21:35:44 +0000

    Clean all first

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,8 +1,8 @@
 module.exports = {
+  all: ['dist/**/*'],
   js: [ 'dist/**/*.js' ],
   css: [ 'dist/**/*.css' ],
   html: [ 'dist/**/*.html' ],
   map: [ 'dist/**/*.map'],
-  images: [ 'dist/images/**/*'],
-  all: ['dist/**/*']
+  images: [ 'dist/images/**/*']
 };
