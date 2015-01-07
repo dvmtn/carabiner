1a7c5843a9aa5d8fb7c7c231a21c05b68a260859
Adam Whittingham <adam.whittingham@gmail.com>
2015-01-07 20:50:18 +0000

    Add grunt-manifest for auto generating manifest.appcache

---
+++ grunt/manifest.js
@@ -0,0 +1,21 @@
+module.exports = {
+  generate: {
+    options: {
+      basePath: 'dist',
+      network: ['http://*', 'https://*'],
+      fallback: ['/ /offline.html'],
+      exclude: ['js/jquery.min.js'],
+      preferOnline: true,
+      verbose: true,
+      timestamp: true,
+      hash: true,
+      master: ['index.html']
+    },
+    src: [
+      '**/*.html',
+      '**/*.min.js',
+      '**/*.css'
+    ],
+    dest: 'dist/manifest.appcache'
+  }
+};
