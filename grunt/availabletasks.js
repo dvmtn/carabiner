5e216828c3590ca4c38aedb1f9b0e49ab787e336
Adam Whittingham <adam.whittingham@gmail.com>
2015-02-24 18:40:44 +0000

    Make "tasks" output have better grouping and descriptions

--- grunt/availabletasks.js
+++ grunt/availabletasks.js
@@ -1,5 +1,18 @@
 module.exports = {
-  availabletasks: {         // task
-    tasks: {}               // target
-  }
+    tasks: {
+      options:{
+        groups: {
+          'main': ['dev', 'test', 'default', 'clean', 'deploy']
+        },
+        filter: 'exclude',
+        tasks: ['availabletasks'],
+        descriptions: {
+          'tasks': 'Show the tasks Carabiner offers',
+          'default': 'Do everything you need to compile your project',
+          'dev': 'Serve the project on port 8080 and process files as they change',
+          'deploy': 'Build and deploy the dist folder. Defaults to Github Pages',
+          'test': 'Run Jasmine tests and jshint'
+        }
+      }
+    }
 };
