2714625e7da884c64562a6f05e21a3d030debcb0
Adam Whittingham <adam.whittingham@gmail.com>
2015-03-10 17:45:40 +0000

    Add build task to make config clearer

--- grunt/availabletasks.js
+++ grunt/availabletasks.js
@@ -7,11 +7,12 @@ module.exports = {
         filter: 'exclude',
         tasks: ['availabletasks'],
         descriptions: {
-          'tasks': 'Show the tasks Carabiner offers',
-          'default': 'Do everything you need to compile your project',
-          'dev': 'Serve the project on port 8080 and process files as they change',
+          'default': 'Run build and test',
+          'build': 'Compile and minify all the things!',
+          'dev': 'Build & Serve the project on port 8080 and process files as they change',
           'deploy': 'Build and deploy the dist folder. Defaults to Github Pages',
-          'test': 'Run Jasmine tests and jshint'
+          'test': 'Run Jasmine tests and jshint',
+          'tasks': 'Show the tasks Carabiner offers'
         }
       }
     }
