bb5c99ed708f04a9d41278ce4c037b5b118e10f9
Anders Fisher <atleastimtrying@gmail.com>
2015-04-05 12:36:12 +0100

    task working with watch for a single jsx file

--- grunt/jsx.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  single_file_output: {
-    files: {
-      'js/editor.js': 'js/jsx/editor.jsx'
-    }
-  }
-};
---
+++ grunt/react.js
@@ -0,0 +1,9 @@
+module.exports = {
+  files: {
+    expand: true,
+    cwd: 'js/jsx/',
+    src: ['**/*.jsx'],
+    dest: 'js/src',
+    ext: '.js'
+  }
+};
